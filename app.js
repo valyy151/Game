468f223203b6c206a70a72236ab0eefc4b3fa44f
valyy151 <marin.valenta1@gmail.com>
2023-03-02 11:53:31 +0100

    Player is moving in the dom

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-class Player {}
+class Player {
+	constructor() {
+		this.positionX = 0;
+		this.positionY = 0;
+		this.player = document.getElementById('player');
+	}
+	moveLeft() {
+		this.positionX--;
+		this.player.style.left = this.positionX + 'vw';
+	}
+
+	moveRight() {
+		this.positionX++;
+		this.player.style.left = this.positionX + 'vw';
+	}
+}
+
+const player = new Player();
+
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'ArrowLeft') {
+		player.moveLeft();
+	} else if (e.key === 'ArrowRight') {
+		player.moveRight();
+	}
+});

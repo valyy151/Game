d3555acc55b4bc355fc719e0c071a518666fd268
valyy151 <marin.valenta1@gmail.com>
2023-03-02 15:10:55 +0100

    Added falling obstacles

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ class Player {
 		this.player = document.getElementById('player');
 	}
 	moveLeft() {
-		this.positionX--;
+		this.positionX -= 2;
 		this.player.style.left = this.positionX + 'vw';
 	}
 
 	moveRight() {
-		this.positionX++;
+		this.positionX += 2;
 		this.player.style.left = this.positionX + 'vw';
 	}
 }
@@ -24,3 +24,35 @@ document.addEventListener('keydown', (e) => {
 		player.moveRight();
 	}
 });
+
+class Obstacle {
+	constructor() {
+		this.positionX = Math.random() * 100 + 'vw';
+		this.positionY = 100;
+
+		this.createObstacle();
+	}
+
+	createObstacle() {
+		this.newObstacle = document.createElement('div');
+		this.newObstacle.className = 'obstacle';
+		this.newObstacle.style.left = this.positionX;
+		const board = document.getElementById('board');
+
+		board.appendChild(this.newObstacle);
+	}
+
+	move() {
+		this.positionY--;
+		this.newObstacle.style.bottom = this.positionY + 'vh';
+	}
+}
+
+const obstacleArray = [];
+
+setInterval(() => {
+	const obstacle = new Obstacle();
+	obstacleArray.push(obstacle);
+}, 1000);
+
+setInterval(() => {}, 50);
